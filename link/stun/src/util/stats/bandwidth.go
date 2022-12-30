f5f7639b4a8db88b0e514786694f008b75f92400
coin0 <coin_st@hotmail.com>
2022-12-30 03:34:40 +0000

    refactor bandwidth stats and add new interfaces to get traffic and reset callback

--- link/stun/src/util/stats/bandwidth.go
+++ link/stun/src/util/stats/bandwidth.go
@@ -12,11 +12,19 @@ type bandwidthCB func(int64, int64)
 type Bandwidth struct {
 	iBytes      atomic.Int64    // bytes
 	oBytes      atomic.Int64    // bytes
+
+	// store previous total I/O traffic at interval
 	interval    time.Duration   // seconds
+	prevIn      int64
+	prevOut     int64
+
 	ech         chan error
 	wg          *sync.WaitGroup
 	once        sync.Once
+
+	// callback function to get called regularly
 	callback    bandwidthCB
+	callbackLck *sync.Mutex
 }
 
 func NewBandwidth(interval time.Duration, cb bandwidthCB) *Bandwidth {
@@ -26,6 +34,7 @@ func NewBandwidth(interval time.Duration, cb bandwidthCB) *Bandwidth {
 		ech: make(chan error),
 		wg: &sync.WaitGroup{},
 		callback: cb,
+		callbackLck: &sync.Mutex{},
 	}
 }
 
@@ -39,30 +48,54 @@ func (bw *Bandwidth) Out(n int) {
 	bw.oBytes.Add(int64(n))
 }
 
+func (bw *Bandwidth) Sum() (int64, int64) {
+
+	return bw.iBytes.Load(), bw.oBytes.Load()
+}
+
+func (bw *Bandwidth) SetCallback(cb bandwidthCB) {
+
+	bw.callbackLck.Lock()
+	defer bw.callbackLck.Unlock()
+
+	bw.callback = cb
+}
+
 func (bw *Bandwidth) Start() {
 
-	run := func() {
+	bw.once.Do(func() {
 
-		bw.wg.Add(1)
-		defer bw.wg.Done()
-		for {
-			ticker := time.NewTicker(bw.interval)
-			select {
-			case <-ticker.C:
-				if bw != nil {
-					bw.callback(bw.iBytes.Swap(0), bw.oBytes.Swap(0))
+		go func() {
+			bw.wg.Add(1)
+			defer bw.wg.Done()
+			for {
+				ticker := time.NewTicker(bw.interval)
+				select {
+				case <-ticker.C:
+					in := bw.iBytes.Load()
+					out := bw.oBytes.Load()
+
+					// invoke callback function
+					bw.callbackLck.Lock()
+					if bw.callback != nil {
+						bw.callback(in - bw.prevIn, out - bw.prevOut)
+					}
+					bw.callbackLck.Unlock()
+
+					// save I/O traffic statistics
+					bw.prevIn = in
+					bw.prevOut = out
+				case <-bw.ech:
+					return
 				}
-			case <-bw.ech:
-				return
 			}
-		}
-	}
-
-	bw.once.Do(func(){ go run() })
+		}()
+	})
 }
 
 func (bw *Bandwidth) Stop() {
 
 	bw.ech <- fmt.Errorf("intentional stop")
 	bw.wg.Wait()
+	bw.once = sync.Once{}
 }
