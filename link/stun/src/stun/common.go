6dab94c84c94e1ff53e79cf943bd96f55aacc342
coin0 <coin_st@hotmail.com>
2023-04-07 08:27:42 +0000

    server: restrict tls version

--- link/stun/src/stun/common.go
+++ link/stun/src/stun/common.go
@@ -189,7 +189,31 @@ func ListenTCP(network, ip, port string) error {
 	if err != nil {
 		return fmt.Errorf("wrong cert or key file path")
 	}
-	tlsconf := &tls.Config{ Certificates: []tls.Certificate{cert} }
+	tlsconf := &tls.Config{
+		Certificates: []tls.Certificate{ cert },
+		MinVersion:   tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		},
+	}
 
 	tcp, err := net.ResolveTCPAddr(network, ip + ":" + port)
 	if err != nil {
